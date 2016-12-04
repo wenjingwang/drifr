a52f1bf174637ea44c0151413e80824a1d90d766
WangWenjing <wenjingwang1990@126.com>
2016-12-05 10:44:16 +1100

    Create an initial project

---
+++ R/drif.R
@@ -0,0 +1,10 @@
+#' Decomposite Recentered Influence Function regression
+#'
+#' @param object a recentered influence function regression model
+#' @param tau quantile
+
+drif <- function(){
+
+
+
+}
