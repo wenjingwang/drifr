2c6160d89b148b37be9e04ee545239631c344808
WangWenjing <wenjingwang1990@126.com>
2016-12-30 10:50:12 +1100

    documenting wage.R

--- R/wage.R
+++ R/wage.R
@@ -1,10 +1,10 @@
 #'@name wage
+#'@title China's Wage Income of Employees in Urban Corporations in 2002
 #'@keywords datasets
-#'
+#'@description This data was selected from China's Household Income Survey 2002
 #'@usage data(wage)
 #'@format A data set with 3866 observations on the following 36 vairables
-#'@source
-#'Data from China's Household Income Survey 2002
+#'
 #'
 #'
 #'
